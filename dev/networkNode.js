3ed6163dbbdcd65af60d64153922d71bc54a764a
ti11o <mr.khikmatillo@gmail.com>
2020-12-22 15:21:05 +0900

    Initial Full

---
+++ dev/networkNode.js
@@ -0,0 +1,274 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const DMChain = require('./dmchain');
+const { v1: uuid} = require('uuid');
+const port = process.argv[2];
+console.log(process.argv[2]);
+const rp = require('request-promise');
+
+const nodeAddress = uuid().split('-').join('');
+
+const dmchain = new DMChain();
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+// get entire blockchain
+app.get('/blockchain', function (req, res) {
+  res.send(dmchain);
+});
+
+
+// create a new transaction
+app.post('/transaction', function(req, res) {
+	const newTransaction = req.body;
+	const blockIndex = dmchain.addTransactionToPendingTransactions(newTransaction);
+	res.json({ note: `Transaction will be added in block ${blockIndex}.` });
+});
+
+
+// broadcast transaction
+app.post('/transaction/broadcast', function(req, res) {
+	const newTransaction = dmchain.createNewTransaction(req.body.amount, req.body.sender, req.body.recipient);
+	dmchain.addTransactionToPendingTransactions(newTransaction);
+
+	const requestPromises = [];
+	dmchain.networkNodes.forEach(networkNodeUrl => {
+		const requestOptions = {
+			uri: networkNodeUrl + '/transaction',
+			method: 'POST',
+			body: newTransaction,
+			json: true
+		};
+
+		requestPromises.push(rp(requestOptions));
+	});
+
+	Promise.all(requestPromises)
+	.then(data => {
+		res.json({ note: 'Transaction created and broadcast successfully.' });
+	});
+});
+
+
+// mine a block
+app.get('/mine', function(req, res) {
+	const lastBlock = dmchain.getLastBlock();
+	const previousBlockHash = lastBlock['hash'];
+	const currentBlockData = {
+		transactions: dmchain.pendingTransactions,
+		index: lastBlock['index'] + 1
+	};
+	const nonce = dmchain.proofOfWork(previousBlockHash, currentBlockData);
+	const blockHash = dmchain.hashBlock(previousBlockHash, currentBlockData, nonce);
+	const newBlock = dmchain.createNewBlock(nonce, previousBlockHash, blockHash);
+
+	const requestPromises = [];
+	dmchain.networkNodes.forEach(networkNodeUrl => {
+		const requestOptions = {
+			uri: networkNodeUrl + '/receive-new-block',
+			method: 'POST',
+			body: { newBlock: newBlock },
+			json: true
+		};
+
+		requestPromises.push(rp(requestOptions));
+	});
+
+	Promise.all(requestPromises)
+	.then(data => {
+		const requestOptions = {
+			uri: dmchain.currentNodeUrl + '/transaction/broadcast',
+			method: 'POST',
+			body: {
+				amount: 12.5,
+				sender: "00",
+				recipient: nodeAddress
+			},
+			json: true
+		};
+
+		return rp(requestOptions);
+	})
+	.then(data => {
+		res.json({
+			note: "New block mined & broadcast successfully",
+			block: newBlock
+		});
+	});
+});
+
+
+// receive new block
+app.post('/receive-new-block', function(req, res) {
+	const newBlock = req.body.newBlock;
+	const lastBlock = dmchain.getLastBlock();
+	const correctHash = lastBlock.hash === newBlock.previousBlockHash; 
+	const correctIndex = lastBlock['index'] + 1 === newBlock['index'];
+
+	if (correctHash && correctIndex) {
+		dmchain.chain.push(newBlock);
+		dmchain.pendingTransactions = [];
+		res.json({
+			note: 'New block received and accepted.',
+			newBlock: newBlock
+		});
+	} else {
+		res.json({
+			note: 'New block rejected.',
+			newBlock: newBlock
+		});
+	}
+});
+
+
+// register a node and broadcast it the network
+app.post('/register-and-broadcast-node', function(req, res) {
+	const newNodeUrl = req.body.newNodeUrl;
+	if (dmchain.networkNodes.indexOf(newNodeUrl) == -1) dmchain.networkNodes.push(newNodeUrl);
+
+	const regNodesPromises = [];
+	dmchain.networkNodes.forEach(networkNodeUrl => {
+		const requestOptions = {
+			uri: networkNodeUrl + '/register-node',
+			method: 'POST',
+			body: { newNodeUrl: newNodeUrl },
+			json: true
+		};
+
+		regNodesPromises.push(rp(requestOptions));
+	});
+
+	Promise.all(regNodesPromises)
+	.then(data => {
+		const bulkRegisterOptions = {
+			uri: newNodeUrl + '/register-nodes-bulk',
+			method: 'POST',
+			body: { allNetworkNodes: [ ...dmchain.networkNodes, dmchain.currentNodeUrl ] },
+			json: true
+		};
+
+		return rp(bulkRegisterOptions);
+	})
+	.then(data => {
+		res.json({ note: 'New node registered with network successfully.' });
+	});
+});
+
+
+// register a node with the network
+app.post('/register-node', function(req, res) {
+	const newNodeUrl = req.body.newNodeUrl;
+	const nodeNotAlreadyPresent = dmchain.networkNodes.indexOf(newNodeUrl) == -1;
+	const notCurrentNode = dmchain.currentNodeUrl !== newNodeUrl;
+	if (nodeNotAlreadyPresent && notCurrentNode) dmchain.networkNodes.push(newNodeUrl);
+	res.json({ note: 'New node registered successfully.' });
+});
+
+
+// register multiple nodes at once
+app.post('/register-nodes-bulk', function(req, res) {
+	const allNetworkNodes = req.body.allNetworkNodes;
+	allNetworkNodes.forEach(networkNodeUrl => {
+		const nodeNotAlreadyPresent = dmchain.networkNodes.indexOf(networkNodeUrl) == -1;
+		const notCurrentNode = dmchain.currentNodeUrl !== networkNodeUrl;
+		if (nodeNotAlreadyPresent && notCurrentNode) dmchain.networkNodes.push(networkNodeUrl);
+	});
+
+	res.json({ note: 'Bulk registration successful.' });
+});
+
+
+// consensus
+app.get('/consensus', function(req, res) {
+	const requestPromises = [];
+	dmchain.networkNodes.forEach(networkNodeUrl => {
+		const requestOptions = {
+			uri: networkNodeUrl + '/blockchain',
+			method: 'GET',
+			json: true
+		};
+
+		requestPromises.push(rp(requestOptions));
+	});
+
+	Promise.all(requestPromises)
+	.then(blockchains => {
+		const currentChainLength = dmchain.chain.length;
+		let maxChainLength = currentChainLength;
+		let newLongestChain = null;
+		let newPendingTransactions = null;
+
+		blockchains.forEach(blockchain => {
+			if (blockchain.chain.length > maxChainLength) {
+				maxChainLength = blockchain.chain.length;
+				newLongestChain = blockchain.chain;
+				newPendingTransactions = blockchain.pendingTransactions;
+			};
+		});
+
+
+		if (!newLongestChain || (newLongestChain && !dmchain.chainIsValid(newLongestChain))) {
+			res.json({
+				note: 'Current chain has not been replaced.',
+				chain: dmchain.chain
+			});
+		}
+		else {
+			dmchain.chain = newLongestChain;
+			dmchain.pendingTransactions = newPendingTransactions;
+			res.json({
+				note: 'This chain has been replaced.',
+				chain: dmchain.chain
+			});
+		}
+	});
+});
+
+
+// get block by blockHash
+app.get('/block/:blockHash', function(req, res) { 
+	const blockHash = req.params.blockHash;
+	const correctBlock = dmchain.getBlock(blockHash);
+	res.json({
+		block: correctBlock
+	});
+});
+
+
+// get transaction by transactionId
+app.get('/transaction/:transactionId', function(req, res) {
+	const transactionId = req.params.transactionId;
+	const trasactionData = dmchain.getTransaction(transactionId);
+	res.json({
+		transaction: trasactionData.transaction,
+		block: trasactionData.block
+	});
+});
+
+
+// get address by address
+app.get('/address/:address', function(req, res) {
+	const address = req.params.address;
+	const addressData = dmchain.getAddressData(address);
+	res.json({
+		addressData: addressData
+	});
+});
+
+
+// block explorer
+app.get('/block-explorer', function(req, res) {
+	res.sendFile('./block-explorer/index.html', { root: __dirname });
+});
+
+
+
+
+
+app.listen(port, function() {
+	console.log(`Listening on port ${port}...`);
+});
